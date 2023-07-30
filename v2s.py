9ca49c7061e45779d5b9ffcc974f6a889ea3f755
Alex <aanikolaev_10@edu.hse.ru>
2023-07-31 01:06:27 +0300

    Code decomposition is completed

--- v2s.py
+++ v2s.py
@@ -2,6 +2,7 @@ import moviepy.editor
 import sys
 import argparse
 import platform
+import time
 
 
 def generate_parser():
@@ -10,6 +11,12 @@ def generate_parser():
     parsrer.add_argument("-n", type=str, help="Target name of the file")
 
     return parsrer
+
+
+def convert_video_to_sound(video_name, audio_name, delim):
+    video_file = moviepy.editor.VideoFileClip(video_name)
+    audio_file = video_file.audio
+    audio_file.write_audiofile(video_name[:video_name.rfind(delim) + 1] + audio_name)
 
 
 def main():
@@ -23,15 +30,14 @@ def main():
         delimiter = "\\"
 
     print("Video2Sound 1.0 Framework")
+    time.sleep(1)
     
     if args.s is None:
         args.s = input("Path to the video to convert: ")
     if args.n is None:
         args.n = input("Title of the audio to be generated: ")
 
-    video_file = moviepy.editor.VideoFileClip(args.s)
-    audio_file = video_file.audio
-    audio_file.write_audiofile(args.s[:args.s.rfind(delimiter) + 1] + args.n)
+    convert_video_to_sound(args.s, args.n, delimiter)
 
     return 0
 
